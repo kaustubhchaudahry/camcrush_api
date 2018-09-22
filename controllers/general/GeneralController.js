6007f72a9cd539be15d281843b9b18afac67f099
kaustubh chaudhary <kc27012@gmail.com>
2018-09-22 06:25:23 -0700

    added validate user

--- controllers/general/GeneralController.js
+++ controllers/general/GeneralController.js
@@ -4,10 +4,33 @@ var Validator = require('validatorjs');
 
 module.exports.currentTimeStamp = async function (data, res) {
 
-    response={};
+    response = {};
 
-    response.timestamp=helper.currentTimeStamp();
+    response.timestamp = helper.currentTimeStamp();
 
     return helper.response_json(200, 'success', res, response);
+};
+
+module.exports.validateUser = async function (data) {
+
+    return new Promise((resolve, reject)=> {
+        var sql = "select * from cc_student where status=1 and id=?";
+
+        con.query(sql, [data.student_id], function (err, result) {
+
+            if (err) {
+                reject({valid: 0})
+            }
+
+            if (result.length > 0) {
+                resolve({valid: 1});
+            } else {
+                reject({valid: 0});
+            }
+
+        });
+
+
+    });
 };
 
--- routes/general_route.js
+++ routes/general_route.js
@@ -2,20 +2,44 @@ var express = require('express');
 var router = express.Router();
 var GeneralController = require('../controllers/general/GeneralController.js');
 var CommonController = require('../controllers/common/CommonController.js');
-
+var Validator = require('validatorjs');
 var con = require('../custom_packages/connection.js');
+var helper = require('../helpers/helper.js');
 
 router.use('/', function (req, res, next) {
     header = req.headers.header_token;
-    CommonController.validateToken(header,res,next);
+    CommonController.validateToken(header, res, next);
 });
 
 router.post('/', function (req, res) {
-res.send('welcome to general route');
+    res.send('welcome to general route');
 });
 
 router.post('/curent-timestamp', function (req, res) {
-   GeneralController.currentTimeStamp({},res);
+    GeneralController.currentTimeStamp({}, res);
+});
+
+router.post('/validate-user', function (req, res) {
+
+    rules = {
+        student_id: 'required',
+    };
+
+    data = req.body;
+
+    var validation = new Validator(data, rules);
+
+    if (validation.fails()) {
+        return helper.response_missing_json('Invalid parameter', res, validation.errors.all());
+    }
+
+    res1 = GeneralController.validateUser(data);
+
+    res1.then((result)=> {
+        return helper.response_json(200, 'Valid User', res, result);
+    }, (result)=> {
+        return helper.response_json(400, "User Don't Have Access", res, result);
+    })
 });
 
 
